c4a33c5ebf659be29ab3bb95d4a4183239c7e57c
Mareike <mareike.brandt@hotmail.de>
2022-05-19 02:07:04 +0900

    feat: init react env

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
 
+// to be able to connect to frontend
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 mongoose.connect(
   "mongodb+srv://mbot:ktQTi9Wt8hrrhGRp@cluster0.dwht6.mongodb.net/mern-project?retryWrites=true&w=majority"
@@ -22,7 +26,7 @@ app.get("/getUsers", (req, res) => {
   });
 });
 
-app.post(/createUser/, async (req, res) => {
+app.post("/createUser", async (req, res) => {
   const user = req.body;
   const newUser = new UserModel(user);
   await newUser.save();
